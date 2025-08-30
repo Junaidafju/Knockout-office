f0611da27c168f7691aa976c9e19e1e66c8893c8
Muhammad Junaid Akhtar <junaidafju@gmail.com>
2025-08-30 18:19:54 +0530

    fixed footer + changed Video + Fixed Loader

--- wp-content/themes/knockout/js/page-loader.js
+++ wp-content/themes/knockout/js/page-loader.js
@@ -34,25 +34,20 @@ class PageLoaderManager {
         this.loader.className = 'page-loader-overlay';
         this.loader.innerHTML = `
             <div class="text-loader">
-                <svg viewBox="0 0 1200 200" xmlns="http://www.w3.org/2000/svg">
-                    <defs>
-                        <!-- Gradient Animation -->
-                        <linearGradient id="text-gradient" x1="0" y1="0" x2="0" y2="1">
-                            <stop stop-color="#973BED" offset="0%" />
-                            <stop stop-color="#b0d136" offset="50%" />
-                            <stop stop-color="#FFC800" offset="100%" />
-                            <animateTransform attributeName="gradientTransform"
-                                type="rotate"
-                                values="0 0 0; 360 0 0"
-                                dur="6s"
-                                repeatCount="indefinite" />
-                        </linearGradient>
-                    </defs>
+                <div class="loader-wrapper">
+                    <span class="loader-letter">K</span>
+                    <span class="loader-letter">N</span>
+                    <span class="loader-letter">O</span>
+                    <span class="loader-letter">C</span>
+                    <span class="loader-letter">K</span>
+                    <span class="loader-letter"></span>
+                    <span class="loader-letter">O</span>
+                    <span class="loader-letter">U</span>
+                    <span class="loader-letter">T</span>
+                    <span class="loader-letter"> </span>
 
-                    <!-- KnockOut Text -->
-                    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" 
-                          class="knockout-text">K N O C K O U T</text>
-                </svg>
+                    <div class="loader"></div>
+                </div>
             </div>
         `;
 
